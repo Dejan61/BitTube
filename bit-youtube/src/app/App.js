b50d8a35690bfdd5a7f1023a1af7fe06a7e44415
Dejan61 <datanackovic212@gmail.com>
2018-06-21 14:45:11 +0200

    search bar

--- bit-youtube/src/app/App.js
+++ bit-youtube/src/app/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../partials/header/Header';
 import Footer from '../partials/footer/Footer';
 import FeedPage from '../app/feed/FeedPage';
+
 import { Route } from 'react-router-dom';
 import './App.css';
 
